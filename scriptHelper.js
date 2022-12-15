cd2a78c29745e0072cbc12f9e1f7526297d76a99
Rocco <roccotrovato126@gmail.com>
2022-12-14 20:02:25 -0500

    Website in working order

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,39 +1,77 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
 
-function validateInput(testInput) {
-   
+function validateInput(input) {
+    if (input === "") {
+        return "Empty";
+    } else if (isNaN(input)) {
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    }
 }
+    
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+function formSubmission(document, pilot, copilot, fuelLevel, cargoLevel) {
+    //console.log(validateInput(cargoLevel))
+    fuelLevel = Number(fuelLevel);
+    cargoLevel = Number(cargoLevel);
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        window.alert("All fields are required");
+    }else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(Number(fuelLevel)) === "Not a Number" || validateInput(Number(cargoLevel)) === "Not a Number") {
+        window.alert("Please enter valid inputs in all fields");
+    }else if  (fuelLevel < 10000){
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("fuelStatus").innerHTML = "Not enough fuel for journey";
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "	#FF0000";
+    }else if (cargoLevel > 10000){
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("cargoStatus").innerHTML = "Too much mass for takeoff";
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "#FF0000";
+    }else {
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
+        document.getElementById("launchStatus").style.color = "#00FF00";
+        document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} Ready`;
+        document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} Ready`;
+    }
+};
+
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
+});
 
     return planetsReturned;
 }
 
-function pickPlanet(planets) {
+
+//TO-DO #1 Add math to pickPlanet to choose random planet.
+function pickPlanet(planet) {
+    let randomizer = Math.random() * 6
+    planet = Math.floor(randomizer)
+    return planet;
+}
+
+
+function addDestinationInfo(document, name, diameter, star, distance, moons, image) {
+   // Here is the HTML formatting for our mission target div.
+document.getElementById("missionTarget").innerHTML =
+                `<h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
+                </ol>
+                <img src=${image}>`
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
